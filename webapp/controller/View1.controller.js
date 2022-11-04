f24ca8f33de114ea09c82a1a719c33e401a45bce
jhy56 <wlgus9976@naver.com>
2022-11-04 15:52:23 +0900

    test

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,29 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
+],
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+    function (Controller, JSONModel) {
+        "use strict";
+
+        return Controller.extend("project1.controller.View1", {
+            onInit: function () {
+                var oData = {
+                    milk : [
+                        { Week : '1주차', Revenue : 1002, Cost : 2323 },
+                        { Week : '2주차', Revenue : 1202, Cost : 2323 },
+                        { Week : '3주차', Revenue : 992, Cost : 2112 },
+                        { Week : '4주차', Revenue : 1002, Cost : 1003 },
+                    ]
+                };
+                var oModel = new JSONModel(oData);
+                this.getView().setModel(oModel);
+
+                var oVizFrame = this.byId("idVizFrame");
+                var oPopOver = this.getView().byId("idPopOver");
+                oPopOver.connect(oVizFrame.getVizUid());
+            }
+        });
+    });
